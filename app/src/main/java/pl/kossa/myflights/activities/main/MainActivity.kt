d8e41949aecf4f36f91f4062c0b3bf38d0cbc3b8
DominikKossinski <dominikkossinski@gmail.com>
2022-01-04 14:47:37 +0100

    handling deepLink provided in notification

--- app/src/main/java/pl/kossa/myflights/activities/main/MainActivity.kt
+++ app/src/main/java/pl/kossa/myflights/activities/main/MainActivity.kt
@@ -1,10 +1,13 @@
 package pl.kossa.myflights.activities.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import pl.kossa.myflights.R
@@ -16,4 +19,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val viewModel: MainActivityViewModel by viewModels()
 
+//    override fun onNewIntent(intent: Intent?) {
+//        super.onNewIntent(intent)
+//        Log.d("MyLog", "Intent: $intent")
+//        findNavController(R.id.listsNavHostFragment).handleDeepLink(intent)
+//    }
 }
