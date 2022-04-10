348f3abaa6e6294672673b35bf964ef255d17daf
DominikKossinski <dominikkossinski@gmail.com>
2022-04-10 13:22:44 +0200

    generating share qr code

--- app/src/main/java/pl/kossa/myflights/dialogs/share/ShareFlightBottomSheet.kt
+++ app/src/main/java/pl/kossa/myflights/dialogs/share/ShareFlightBottomSheet.kt
@@ -1,12 +1,14 @@
 package pl.kossa.myflights.dialogs.share
 
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
+import net.glxn.qrgen.android.QRCode
+import pl.kossa.myflights.R
 import pl.kossa.myflights.architecture.dialogs.BaseBottomSheet
 import pl.kossa.myflights.databinding.DialogShareFlightBinding
+import pl.kossa.myflights.exstensions.dpToPx
 
 @AndroidEntryPoint
 class ShareFlightBottomSheet : BaseBottomSheet<ShareFlightViewModel, DialogShareFlightBinding>() {
@@ -17,7 +19,9 @@ class ShareFlightBottomSheet : BaseBottomSheet<ShareFlightViewModel, DialogShare
         super.collectFlow()
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             viewModel.isLoadingData.collectLatest {
-                binding.progressBar.isVisible = it
+                if (it) {
+                    binding.qrIv.setImageResource(R.drawable.progress_placeholder)
+                }
             }
         }
         viewLifecycleOwner.lifecycleScope.launchWhenStarted {
@@ -28,6 +32,11 @@ class ShareFlightBottomSheet : BaseBottomSheet<ShareFlightViewModel, DialogShare
     }
 
     private fun setupSharedFlightId(sharedFlightId: String) {
-        binding.infoTv.text = sharedFlightId
+        val size = requireContext().dpToPx(200f).toInt()
+        val qrCodeBitmap =
+            QRCode.from(getString(R.string.share_flight_uri_format, sharedFlightId))
+                .withSize(size, size)
+                .bitmap()
+        binding.qrIv.setImageBitmap(qrCodeBitmap)
     }
 }
