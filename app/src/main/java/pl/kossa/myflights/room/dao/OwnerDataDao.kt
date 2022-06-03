69677534c71ffe9410616ddc497b22d02426aa53
DominikKossinski <dominikkossinski@gmail.com>
2022-06-03 22:54:00 +0200

    owner data dao

---
+++ app/src/main/java/pl/kossa/myflights/room/dao/OwnerDataDao.kt
@@ -0,0 +1,25 @@
+package pl.kossa.myflights.room.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import pl.kossa.myflights.room.entities.ImageModel
+import pl.kossa.myflights.room.entities.OwnerData
+import pl.kossa.myflights.room.entities.OwnerDataModel
+
+@Dao
+abstract class OwnerDataDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    protected abstract suspend fun insertOwnerDataModel(ownerDataModel: OwnerDataModel)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    protected abstract suspend fun insertImageModel(imageModel: ImageModel)
+
+    suspend fun insertOwnerData(ownerData: OwnerData) {
+        ownerData.image?.let {
+            insertImageModel(it)
+        }
+        insertOwnerDataModel(ownerData.ownerData)
+    }
+}
