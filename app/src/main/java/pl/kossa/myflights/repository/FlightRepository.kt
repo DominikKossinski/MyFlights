5f7ce772e8bec3db9332afd9cdae65045c4b0d60
DominikKossinski <dominikkossinski@gmail.com>
2022-06-08 22:34:22 +0200

    flights repository

--- app/src/main/java/pl/kossa/myflights/repository/FlightRepository.kt
+++ app/src/main/java/pl/kossa/myflights/repository/FlightRepository.kt
@@ -1,4 +1,54 @@
 package pl.kossa.myflights.repository
 
-class FlightRepository {
+import pl.kossa.myflights.api.requests.FlightRequest
+import pl.kossa.myflights.api.services.FlightsService
+import pl.kossa.myflights.room.dao.FlightDao
+import pl.kossa.myflights.room.entities.Flight
+import pl.kossa.myflights.utils.PreferencesHelper
+
+class FlightRepository(
+    private val flightsService: FlightsService,
+    private val flightDao: FlightDao,
+    private val preferencesHelper: PreferencesHelper
+) {
+
+    suspend fun getFlights(): List<Flight> {
+        val response = flightsService.getAllFlights()
+        val flights = response.body ?: emptyList()
+        flights.forEach {
+            flightDao.insertFlight(Flight.fromApiFlight(it))
+        }
+        return flightDao.getAll()
+    }
+
+    suspend fun getFlightById(flightId: String): Flight? {
+        val response = flightsService.getFLightById(flightId)
+        response.body?.let {
+            flightDao.insertFlight(Flight.fromApiFlight(it))
+        }
+        return flightDao.getFlightById(flightId)
+    }
+
+    suspend fun createFlight(flightRequest: FlightRequest): String? {
+        val response = flightsService.postFlight(flightRequest)
+        response.body?.entityId?.let {
+            getFlightById(it)
+        }
+        return response.body?.entityId
+    }
+
+    suspend fun saveFlight(flightId: String, flightRequest: FlightRequest) {
+        flightsService.putFlight(flightId, flightRequest)
+        getFlightById(flightId)
+    }
+
+    suspend fun deleteFlight(flightId: String) {
+        flightsService.deleteFlight(flightId)
+        val flight = flightDao.getFlightById(flightId)
+        flight?.let { f ->
+            preferencesHelper.userId?.let { userId ->
+                flightDao.delete(userId, f)
+            }
+        }
+    }
 }
