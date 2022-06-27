7997dfc2a439a8e2da6a3288ee062789b29a8c35
DominikKossinski <dominikkossinski@gmail.com>
2022-06-27 23:41:34 +0200

    small refactor

--- app/src/main/java/pl/kossa/myflights/repository/AirportRepository.kt
+++ app/src/main/java/pl/kossa/myflights/repository/AirportRepository.kt
@@ -2,6 +2,7 @@ package pl.kossa.myflights.repository
 
 import pl.kossa.myflights.api.call.ApiResponse1
 import pl.kossa.myflights.api.requests.AirportRequest
+import pl.kossa.myflights.api.responses.HttpCode
 import pl.kossa.myflights.api.services.AirportsService
 import pl.kossa.myflights.architecture.BaseRepository
 import pl.kossa.myflights.architecture.ResultWrapper
@@ -46,12 +47,24 @@ class AirportRepository(
                 airportDao.insertAirport(Airport.fromApiAirport(it))
             }
         }
-        val value = preferencesHelper.userId?.let { airportDao.getAirportById(it, airportId) }
+        val airport =
+            preferencesHelper.userId?.let { airportDao.getAirportById(it, airportId) }
         return when (response) {
-            is ApiResponse1.Success -> ResultWrapper.Success(value)
-            is ApiResponse1.GenericError -> ResultWrapper.GenericError(value, response.apiError)
+            is ApiResponse1.Success -> {
+                ResultWrapper.Success(airport)
+            }
+            is ApiResponse1.GenericError -> {
+                when (response.apiError.code) {
+                    HttpCode.NOT_FOUND.code -> {
+                        airport?.let { airportDao.delete(it) }
+                        ResultWrapper.GenericError(null, response.apiError)
+                    }
+                    else ->
+                        ResultWrapper.GenericError(airport, response.apiError)
+                }
+            }
             is ApiResponse1.NetworkError -> ResultWrapper.NetworkError(
-                value,
+                airport,
                 response.networkErrorType
             )
         }
